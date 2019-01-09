1570be32a21e2c204852c7b45e96f9bee744f552
Jelle De Loecker <jelle@develry.be>
2019-01-09 23:28:45 +0100

    Update copyright dates for 2019

--- app/config/bootstrap.js
+++ app/config/bootstrap.js
@@ -1,11 +1,11 @@
 /**
  * Alchemy: Node.js MVC Framework
- * Copyright 2013-2018
+ * Copyright 2013-2019
  *
  * Licensed under The MIT License
  * Redistributions of files must retain the above copyright notice.
  *
- * @copyright   Copyright 2013-2018
+ * @copyright   Copyright 2013-2019
  * @since       0.1.0
  * @license     MIT License (http://www.opensource.org/licenses/mit-license.php)
  */
