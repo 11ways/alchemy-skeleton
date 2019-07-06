63c97040d818bac0c76062a219d14b817d145f20
Jelle De Loecker <jelle@develry.be>
2019-07-06 17:17:00 +0200

    Use __alchemy_project_name__ placeholder in database config

--- app/config/dev/database.js
+++ app/config/dev/database.js
@@ -1,7 +1,7 @@
 // Create the 'default' datasource of type 'mongo'
 Datasource.create('mongo', 'default', {
 	host      : '127.0.0.1',
-	database  : 'alchemy',
+	database  : '__alchemy_project_name___dev',
 	login     : false,
 	password  : false
 });
--- app/config/live/database.js
+++ app/config/live/database.js
@@ -1,7 +1,7 @@
 // Create the 'default' datasource of type 'mongo'
 Datasource.create('mongo', 'default', {
 	host      : '127.0.0.1',
-	database  : 'alchemy',
+	database  : '__alchemy_project_name___live',
 	login     : false,
 	password  : false
 });
