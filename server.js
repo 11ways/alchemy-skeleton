be478ad180c03354e28fd04c8eb5f33b84d6b958
Jelle De Loecker <jelle@elevenways.be>
2020-07-22 15:04:17 +0200

    Update for alchemy v1.1.0

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
 /**
  * Alchemy: a node.js framework
- * Copyright 2014-2019, Jelle De Loecker
+ * Copyright 2013-2020, Jelle De Loecker
  *
  * Licensed under The MIT License
  * Redistributions of files must retain the above copyright notice.
  *
- * @copyright     Copyright 2014-2019, Jelle De Loecker
+ * @copyright     Copyright 2013-2020, Jelle De Loecker
  * @link          
  * @license       MIT License (http://www.opensource.org/licenses/mit-license.php)
  */
