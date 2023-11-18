face23e2b8e2d19347e9d78efd0f9c6a42288cf7
Jelle De Loecker <jelle@elevenways.be>
2023-11-18 12:51:30 +0100

    📝 Do a late copyright year update

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
 /**
  * Alchemy: a node.js framework
- * Copyright 2013-2022, Jelle De Loecker
+ * Copyright 2013-2023, Jelle De Loecker
  *
  * Licensed under The MIT License
  * Redistributions of files must retain the above copyright notice.
  *
- * @copyright     Copyright 2013-2022, Jelle De Loecker
+ * @copyright     Copyright 2013-2023, Jelle De Loecker
  * @link          
  * @license       MIT License (http://www.opensource.org/licenses/mit-license.php)
  */
